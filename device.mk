3ebad22c14df568b4b20097faa8c0dc04648397b
TH779 <i@779.moe>
2021-08-06 19:05:15 +0800

    marble: Import HotwordEnrollment permissions XML
     * Extracted from the privapp-permissions-google-product.xml file of
       V14.0.1.0.TMRMIXM.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I872707a9633c087b119bac2f5bb07891969d7ae3

--- device.mk
+++ device.mk
@@ -235,6 +235,11 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
+# HotwordEnrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
