867e184d6ebb2a39d86d92731a97a04f2a51cb01
Vivekachooz <vivekachooz@gmail.com>
2022-02-21 17:27:43 +0530

    marble: XiaomiDoze: Declare presence of wake-up proximity sensor

--- parts/src/org/lineageos/settings/sensors/ProximitySensor.java
+++ parts/src/org/lineageos/settings/sensors/ProximitySensor.java
@@ -52,7 +52,7 @@ public class ProximitySensor implements SensorEventListener {
     public ProximitySensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         mExecutorService = Executors.newSingleThreadExecutor();
     }
 
