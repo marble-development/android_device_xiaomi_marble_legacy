e9b84ce08629808a063792221a602c6d68b322ac
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    marble: Update to Boot Control HAL v1.2

--- device.mk
+++ device.mk
@@ -29,9 +29,9 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
