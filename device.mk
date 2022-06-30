f8608817ec2ab423c5c198d8122d3c74bf2a9491
Arian <arian.kulmer@web.de>
2022-06-30 17:41:25 +0200

    marble: Build qti aidl power hal
    Change-Id: I7c9352e26653c51401c6825ca8988e7cb05479bc

--- device.mk
+++ device.mk
@@ -115,6 +115,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # RIL
 PRODUCT_PACKAGES += \
     Ims \
