c4534a8f257bec24e8a56187de7edb444a3fea16
althafvly <althafvly@gmail.com>
2022-09-20 11:16:46 +0530

    marble: libinit: Set default USB and Bluetooth name
    Change-Id: I65948d08d95b138d9faed262ad3376305b9510a7

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -34,11 +34,11 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("marketname", variant.marketname, true);
     set_ro_build_prop("model", variant.model, true);
     set_ro_build_prop("mod_device", variant.model, true);
-
+    property_override("vendor.usb.product_string", variant.marketname, true);
     if (access("/system/bin/recovery", F_OK) != 0) {
         set_ro_build_prop("fingerprint", variant.build_fingerprint);
         property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
-
+        property_override("bluetooth.device.default_name", variant.marketname, true);
         property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
     }
 }
