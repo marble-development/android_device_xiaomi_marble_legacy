426da06fbbfa49879e9beb89da992fcfba3c6192
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-06-04 09:44:56 +0800

    marble: Add patch for vibrator & sysbta

---
+++ patch/patch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+PATCH_LOC=$PWD/device/xiaomi/marble/patch
+cd frameworks/native
+git am $PATCH_LOC/frameworks/native/0001-Fix-vibration.patch
+cd ../av
+git am $PATCH_LOC/frameworks/av/0001-APM-Optionally-force-load-audio-policy-for-system-si.patch
+git am $PATCH_LOC/frameworks/av/0002-APM-Remove-A2DP-audio-ports-from-the-primary-HAL.patch
+cd ../../packages/modules/Bluetooth
+git am $PATCH_LOC/packages/modules/Bluetooth/0001-audio_hal_interface-Optionally-use-sysbta-HAL.patch
+
