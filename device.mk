f256c12672d66ae08c0b7b944a0ef801d0aeaf5a
Akash Srivastava <akashniki@gmail.com>
2023-06-14 11:20:31 +0300

    marble: Build some missing LDAC libs from source
    * Bluetooth libs look for them
    
    Change-Id: 7116f4470c5e19d6739acbe0d5d7e6fb6ca9e9ec
    Signed-off-by: frozenking <frozenking1240196@gmail.com>

--- device.mk
+++ device.mk
@@ -106,7 +106,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    libldacBT_abr \
+    libldacBT_enc
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
