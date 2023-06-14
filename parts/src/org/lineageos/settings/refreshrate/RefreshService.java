f8bfbe3c671195cf4b72f8ae14cae4d9fdfca9ac
LuK1337 <priv.luk@gmail.com>
2023-06-14 21:27:44 +0900

    marble: Partially import parts from raphael & sm8350-common
    Updates to:
    
    * Dirac
    * Doze
    
    Added:
    
    * Thermals
    * Per-app refresh rate
    * Clear speaker
    
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: nullxception <enuma.alrizky@gmail.com>
    Co-authored-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>

---
+++ parts/src/org/lineageos/settings/refreshrate/RefreshService.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.refreshrate;
+
+import android.app.ActivityManager;
+import android.app.ActivityTaskManager;
+import android.app.ActivityTaskManager.RootTaskInfo;
+import android.app.IActivityTaskManager;
+import android.app.Service;
+import android.app.TaskStackListener;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.os.RemoteException;
+
+public class RefreshService extends Service {
+
+    private static final String TAG = "RefreshService";
+    private static final boolean DEBUG = false;
+
+    private String mPreviousApp;
+    private RefreshUtils mRefreshUtils;
+    private IActivityTaskManager mActivityTaskManager;
+    private final TaskStackListener mTaskListener = new TaskStackListener() {
+        @Override
+        public void onTaskStackChanged() {
+            try {
+                final RootTaskInfo info = mActivityTaskManager.getFocusedRootTaskInfo();
+                if (info == null || info.topActivity == null) {
+                    return;
+                }
+                String foregroundApp = info.topActivity.getPackageName();
+                if (!foregroundApp.equals(mPreviousApp)) {
+                    mRefreshUtils.setRefreshRate(foregroundApp);
+                    mPreviousApp = foregroundApp;
+                }
+            } catch (Exception e) {}
+            }
+        };
+
+    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mPreviousApp = "";
+            mRefreshUtils.setDefaultRefreshRate(context);
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+        mRefreshUtils = new RefreshUtils(this);
+        mRefreshUtils.setDefaultRefreshRate(this);
+        try {
+            mActivityTaskManager = ActivityTaskManager.getService();
+            mActivityTaskManager.registerTaskStackListener(mTaskListener);
+        } catch (RemoteException e) {
+            // Do nothing
+        }
+        registerReceiver();
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        unregisterReceiver();
+        try {
+            ActivityTaskManager.getService().unregisterTaskStackListener(mTaskListener);
+        } catch (RemoteException e) {
+            // Do nothing
+        }
+        mRefreshUtils.setDefaultRefreshRate(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void registerReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        this.registerReceiver(mIntentReceiver, filter);
+    }
+
+    private void unregisterReceiver() {
+        this.unregisterReceiver(mIntentReceiver);
+    }
+}
