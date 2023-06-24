11f39bfc21736f48d7b5a92108dfd8194e478437
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-06-24 20:54:56 +0800

    marble: Try to fix battery usage

--- patch/patch.sh
+++ patch/patch.sh
@@ -5,6 +5,8 @@ git am $PATCH_LOC/frameworks/native/0001-Fix-vibration.patch
 cd ../av
 git am $PATCH_LOC/frameworks/av/0001-APM-Optionally-force-load-audio-policy-for-system-si.patch
 git am $PATCH_LOC/frameworks/av/0002-APM-Remove-A2DP-audio-ports-from-the-primary-HAL.patch
+cd ../base
+git am $PATCH_LOC/frameworks/base/0001-Revert-Use-getUahDischarge-when-available.patch
 cd ../../packages/modules/Bluetooth
 git am $PATCH_LOC/packages/modules/Bluetooth/0001-audio_hal_interface-Optionally-use-sysbta-HAL.patch
 
