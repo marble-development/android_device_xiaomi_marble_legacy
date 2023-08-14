a6c17d0aa1faa148a50a0533808ec87340979c3b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-14 08:01:05 +0530

    marble: rootdir: Set correct permissions for pocket sensor
    Until we enable proximity sensor for pocket check (TODO later),
    we are using large area touch detection. Set correct permissions
    for sensor HAL to read the touchpanel node.

--- device.mk
+++ device.mk
@@ -407,6 +407,7 @@ PRODUCT_PACKAGES += \
     charger_fw_fstab.qti \
     fstab.qcom \
     init.class_main.sh \
+    init.marble.rc \
     init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -108,3 +108,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd-odm.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.marble.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.marble.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/
+include $(BUILD_PREBUILT)
