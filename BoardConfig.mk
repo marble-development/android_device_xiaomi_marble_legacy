809018829f9b1303bf7f039e069ada56201fbece
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-08-17 10:40:27 +0800

    marble: Import config.fs
    * from https://git.codelinaro.org/clo/la/device/qcom/common/-/blob/qcom-devices.lnx.7.0.r1-rel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,6 +58,9 @@ TARGET_SCREEN_DENSITY := 440
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config/config.fs
+
 # Kernel
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE := 0x00000000
