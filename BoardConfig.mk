bb5da551952683840244454a9547dfaee4f20165
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-08-20 20:46:58 +0800

    marble: Add swinfo.fingerprint cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,6 +91,7 @@ BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
     bootinfo.fingerprint=$(LINEAGE_VERSION) \
+    swinfo.fingerprint=$(LINEAGE_VERSION) \
     mtdoops.fingerprint=$(LINEAGE_VERSION)
 
 BOARD_BOOTCONFIG := \
