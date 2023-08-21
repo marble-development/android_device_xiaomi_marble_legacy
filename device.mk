494b11de2ff7c3c6cf438a4cc3b4a0e2bb781f35
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-08-21 22:46:12 +0800

    marble: Build xiaomi vibrator
    * from https://github.com/AOSPA/android_hardware_xiaomi/tree/topaz/aidl/vibrator

--- device.mk
+++ device.mk
@@ -539,10 +539,10 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VENDOR API
 BOARD_API_LEVEL := 31
