52ff54cf508cae5bdc6d609990741131000e9722
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-08-22 20:11:10 +0800

    marble: Build android.frameworks.sensorservice@1.0
    08-22 20:00:33.554  1940  1940 W HidlServiceManagement: Waited one second for android.frameworks.sensorservice@1.0::ISensorManager/default
    08-22 20:00:33.555   721   721 I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    08-22 20:00:33.555  1940  1940 I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    08-22 20:00:33.555   721  3466 W libc    : Unable to set property "ctl.interface_start" to "android.frameworks.sensorservice@1.0::ISensorManager/default": error code: 0x20
    08-22 20:00:33.555   721  3466 I hwservicemanager: Tried to start android.frameworks.sensorservice@1.0::ISensorManager/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.

--- device.mk
+++ device.mk
@@ -430,6 +430,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
+    android.frameworks.sensorservice@1.0 \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
