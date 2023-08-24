346e2d5c5aa8a7b692b17377332954fb4e52a444
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-08-24 12:08:48 +0800

    marble: Add missing blobs for audio.primary.taro.so
    * this fixes the no sound on phone calls.

--- device.mk
+++ device.mk
@@ -93,6 +93,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_ukee/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_ukee_qssi/audio_policy_configuration.xml
 
 # Automotive
