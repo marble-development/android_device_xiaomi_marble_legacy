9d9ec5100cf476e9ba07f9dfe07c3aabe0adcf0f
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-08-25 08:51:50 +0800

    marble: Drop bootinfo.fingerprint
    03-15 10:36:38.556     1     1 W bootinfo: unknown parameter 'fingerprint' ignored

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,6 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
-    bootinfo.fingerprint=$(LINEAGE_VERSION) \
     swinfo.fingerprint=$(LINEAGE_VERSION) \
     mtdoops.fingerprint=$(LINEAGE_VERSION)
 
