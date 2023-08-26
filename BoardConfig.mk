1df5d9396c770d3e6988ae661179bbf6e5447231
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-08-26 08:21:30 +0800

    marble: Initial sepolicy rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -220,6 +220,8 @@ VENDOR_SECURITY_PATCH := 2023-08-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor boot
