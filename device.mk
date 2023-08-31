f55c715fd16beef26b282c7da594e69941320077
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-08-31 23:49:58 +0800

    marble: Do not set adb secure by default

--- device.mk
+++ device.mk
@@ -400,10 +400,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
-
-# Properties
-include $(LOCAL_PATH)/configs/properties/default.mk
-
 # QTI
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.systemhelper@1.0.vendor
