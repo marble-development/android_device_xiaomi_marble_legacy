623bacec78a02dddcc804c21ecb63437cbf237a3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-17 21:56:39 +0900

    marble: sepolicy: Update entries
    * from https://github.com/ghostrider-reborn/android_device_xiaomi_marble/tree/topaz/sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,7 +226,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/dolby
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor boot
