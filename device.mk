d175bb9764ed4dc719a93a5cd50c740a61e45162
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-04 20:36:50 +0530

    marble: Switch to Wifi service AIDL

--- device.mk
+++ device.mk
@@ -598,7 +598,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     android.hardware.wifi.hostapd@1.0.vendor \
     hostapd \
     libwpa_client \
