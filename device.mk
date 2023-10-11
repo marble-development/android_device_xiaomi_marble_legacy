b7fd4a5352a2de7fdc105d94fe5024f91f92cab5
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-10-11 15:55:34 +0800

    marble: overlay: Import CarrierConfig from PA
    * from https://github.com/ghostrider-reborn/android_device_xiaomi_marble/blob/topaz/overlay/MarbleCarrierConfigOverlay/res/xml/vendor.xml

--- device.mk
+++ device.mk
@@ -38,6 +38,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AospWifiResOverlayMarble \
     ApertureResOverlay \
+    CarrierConfigResMarble \
     FrameworksResCommon \
     FrameworksResOverlayMarble \
     SettingsOverlayMarble \
