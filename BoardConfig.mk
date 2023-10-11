ba825a6215fb69cfc87a528c6da04a6a5404eda9
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-10-12 00:02:27 +0800

    marble: Initial sepolicy and go enforcing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -205,6 +205,10 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+
 # VINTF
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += := \
