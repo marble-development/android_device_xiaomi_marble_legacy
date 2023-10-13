d45b4ef07fd2cf4034ebfa4c3fcb89fc5cf98407
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-10-13 21:25:01 +0800

    marble: Switch to android.hardware.health@2.1-impl
    05-04 02:17:10.867  2673  2673 F android.hardware.health@2.1-service: Check failed: passthrough != nullptr Cannot find passthrough implementation of health 2.1 HAL for instance default

--- device.mk
+++ device.mk
@@ -228,8 +228,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
-    android.hardware.health@2.1-impl-qti.recovery \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # HIDL
