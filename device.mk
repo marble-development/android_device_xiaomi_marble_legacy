b0953d5cf6beeacaaf111dab40f2c187c9eb2372
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-10-14 13:32:55 +0800

    marble: Build libsdedrm from source

--- device.mk
+++ device.mk
@@ -155,6 +155,7 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libgpu_tonemapper \
     libqservice \
+    libsdedrm \
     libsdmcore \
     libsdmutils \
     vendor.display.config@1.11.vendor \
