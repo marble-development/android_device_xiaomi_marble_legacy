e4c7b7f7fae847c5b972fada3308b7fdc54f1883
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-10-15 10:28:28 +0800

    marble: Update to android.hardware.power@1.3

--- device.mk
+++ device.mk
@@ -423,7 +423,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
+    android.hardware.power@1.3.vendor \
     libgrpc++_unsecure.vendor
 
 PRODUCT_COPY_FILES += \
